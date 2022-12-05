ca2730057de560ef85d460170df0458d62674887
linhvu <linh.vup@gmail.com>
2022-12-05 10:42:25 +0000

    add min year and styling to date picker

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,17 @@
 import DatePicker from 'react-datepicker';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import 'react-datepicker/dist/react-datepicker.css';
 
 function Home() {
   const date = new Date();
   const [startDate, setStartDate] = useState(date);
   const navigate = useNavigate();
+
+  const subtractYears = (date, years) => {
+    date.setFullYear(date.getFullYear() - years);
+    return date;
+  };
 
   const handleChange = (date) => {
     setStartDate(date);
@@ -20,21 +26,22 @@ function Home() {
     <>
       <section className="hero is-small is-link">
         <div className="hero-body">
-          <p className="title">Your birth year in songs</p>
-          <p className="subtitle">Discover music from your year of birth</p>
+          <p className="title">Discover music from a random year</p>
+          {/* <p className="subtitle">Discover music from your year of birth</p> */}
         </div>
       </section>
       <section className="section">
         <div className="container">
           <div className="columns">
             <div className="column is-half">
-              <h1 className="title">Pick your birthyear</h1>
+              <h1 className="title">Pick a year</h1>
               <div className="is-inline-flex">
                 <DatePicker
                   selected={startDate}
                   onChange={handleChange}
                   showYearPicker
                   dateFormat="yyyy"
+                  minDate={subtractYears(date, 22)}
                   maxDate={new Date()}
                 />
                 <button onClick={handleSelect}>Get My Random Song</button>
